1b7452a0043472bc375024b334bee410ee0737e4
adilijaz <adilijaz.02@gmail.com>
2021-09-13 00:51:50 +0500

    Add Fee Model and fee route

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,7 @@
 // Packages
 const express = require('express')
 // Routes
+const fee = require('../routes/fee')
 const student = require('../routes/student')
 const users = require('../routes/users')
 const auth = require('../routes/auth')
@@ -9,6 +10,7 @@ const error = require('../middlewares/error')
 
 module.exports = function(app){
     app.use(express.json())
+    app.use('./api/fee',fee)
     app.use('/api/students', student)
     app.use('/api/users', users)
     app.use('/api/login', auth)
